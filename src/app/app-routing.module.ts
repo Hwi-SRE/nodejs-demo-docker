252e215e2e68151296c2d5bde7347584d139ada0
Hwi-SRE <136054509+Hwi-SRE@users.noreply.github.com>
2026-06-16 11:10:33 +0300

    Remove RouterModule import from AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { UnitsComponent } from './units/units.component';
 const routes: Routes = [{ path: "", component: HomeComponent },{ path: "units", component: UnitsComponent }, { path: "unit-details/:id", component: UnitDetailsComponent },];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
